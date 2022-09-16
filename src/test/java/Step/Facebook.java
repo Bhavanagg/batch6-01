c4fd2be5a488ef84babd11fa2f7c877d4d6e0a63
Bhavanagg <bhavanareddygogulamudi@gmail.com>
2022-09-16 16:19:48 +0530

    first commit

---
+++ src/test/java/Step/Facebook.java
@@ -0,0 +1,37 @@
+package Step;
+import java.util.Map;
+import com.beust.jcommander.internal.Maps;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Facebook {
+	@Given("Enter Facebook login page")
+	public void enter_facebook_login_page() {
+	    
+		System.out.println("login successful");	
+	}
+	@When("Enter username and password {string},{string}")
+	public void enter_username_and_password(String username, String password) {
+	   
+		System.out.println(username);	
+		System.out.println(password);	
+		
+	}
+
+   @Then("Navigate to login fuction")
+	public void navigate_to_login_fuction() {
+	    
+		System.out.println("done");	
+		
+	}
+	@When("Enter username and password")
+	public void enter_username_and_password(io.cucumber.datatable.DataTable dataTable) {
+	for(Map<String,String> testdata : dataTable.asMaps()) {
+		System.out.println("username" +testdata.get("username"));	
+		System.out.println("password" +testdata.get("password"));	
+		
+	}
+	
+}
+}
